b2cc6131e6f04c8d4c3cd755d88559dfd69dd67d
ilzira <mondayseptember@yandex.ru>
2023-12-13 00:20:44 +0500

    KS-3-2 решение 4 задачи 3 урока

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+    var stepWhereFrom = "E2"
+    var stepWhereTo = "E4"
+    var moveNumber = "1"
+
+    println("$stepWhereFrom - $stepWhereTo; $moveNumber")
+
+    stepWhereFrom = "D2"
+    stepWhereTo = "D3"
+    moveNumber = "2"
+    println("$stepWhereFrom - $stepWhereTo; $moveNumber")
+}
+
