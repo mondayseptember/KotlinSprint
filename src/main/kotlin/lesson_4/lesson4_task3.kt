b49ccbac21470db9e5cdd376d1d0a46510211c59
ilzira <mondayseptember@yandex.ru>
2023-12-14 22:29:30 +0500

    KS-4-3 исправление решения 3 задачи

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,10 +1,15 @@
 package lesson_4
 
 fun main() {
-    val weatherIsSunny = true
-    val tentIsOpened = true
-    val airHumidity = 20
-    val currentSeason = "winter"
-
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(weatherIsSunny == true) && (tentIsOpened == true) && (airHumidity == 20) && (currentSeason != "winter")}")
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых? ${(WEATHER_IS_SUNNY == true) && 
+                    (TENT_IS_OPENED == true) &&
+                    (AIR_HUMIDITY == 20) &&
+                    (CURRENT_SEASON != "winter")}"
+    )
 }
+
+const val WEATHER_IS_SUNNY = true
+const val TENT_IS_OPENED = true
+const val AIR_HUMIDITY = 20
+const val CURRENT_SEASON = "winter"
