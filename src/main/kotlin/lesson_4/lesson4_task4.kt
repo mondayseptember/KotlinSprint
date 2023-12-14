ffe371a63c34c21b1eedc07b85a9ee0d80a4e515
ilzira <mondayseptember@yandex.ru>
2023-12-14 22:49:06 +0500

    KS-4-4 исправление решения 4 задачи

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,13 +2,14 @@ package lesson_4
 
 fun main() {
     val dayOfTraining = 5
-    val evenDays = 0
-    val oddDays = 1
+    val evenDays = dayOfTraining % 2 == 0
+    val oddDays = !evenDays
 
-    println("""    
-    Упражнения для рук:       ${dayOfTraining % 2 == oddDays}
-    Упражнения для ног:       ${dayOfTraining % 2 == evenDays}
-    Упражнения для спины:  ${dayOfTraining % 2 == evenDays}
-    Упражнения для пресса: ${dayOfTraining % 2 == oddDays}
-    """.trimIndent())
+    println("""  
+    Упражнения для рук:       $oddDays
+    Упражнения для ног:       $evenDays
+    Упражнения для спины:  $evenDays
+    Упражнения для пресса: $oddDays
+    """.trimIndent()
+    )
 }
