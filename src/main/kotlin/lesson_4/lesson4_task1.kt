e6999bf1937056546b2e4dc764e3efa8a43da11c
ilzira <mondayseptember@yandex.ru>
2023-12-16 14:07:22 +0500

    KS-4-1 улучшила читабельность кода

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -4,9 +4,8 @@ fun main() {
     val bookedTablesForToday = 13
     val bookedTablesForTomorrow = 9
 
-    println(
-        "Доступность столиков на сегодня: ${bookedTablesForToday < NUMBER_OF_TABLES}" + "\n" + "Доступность столиков на завтра: ${bookedTablesForTomorrow < NUMBER_OF_TABLES}"
-    )
+    println("Доступность столиков на сегодня: ${bookedTablesForToday < NUMBER_OF_TABLES}")
+    println("Доступность столиков на завтра: ${bookedTablesForTomorrow < NUMBER_OF_TABLES}")
 }
 
 const val NUMBER_OF_TABLES = 13
