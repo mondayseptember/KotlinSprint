f91a8c25564c46746ea3e9606f5b9d7e4ba31771
ilzira <mondayseptember@yandex.ru>
2023-12-16 14:47:59 +0500

    KS-4-2 добавлены константы и переносы для длинных строк

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,16 +1,24 @@
 package lesson_4
 
 fun main() {
-    val minWeightForAverage = 35
-    val maxWeightForAverage = 100
-    val maxVolumeForAverage = 100
-
     var weight = 20
     var volume = 80
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight > minWeightForAverage && weight <= maxWeightForAverage) && (volume < maxVolumeForAverage)}")
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${
+            (weight > MIN_WEIGHT_FOR_AVERAGE && weight <= MAX_WEIGHT_FOR_AVERAGE) && (volume < MAX_VOLUME_FOR_AVERAGE)
+        }"
+    )
 
     weight = 50
     volume = 100
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight > minWeightForAverage && weight <= maxWeightForAverage) && (volume < maxVolumeForAverage)}")
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${
+            (weight > MIN_WEIGHT_FOR_AVERAGE && weight <= MAX_WEIGHT_FOR_AVERAGE) && (volume < MAX_VOLUME_FOR_AVERAGE)
+        }"
+    )
 }
+
+const val MIN_WEIGHT_FOR_AVERAGE = 35
+const val MAX_WEIGHT_FOR_AVERAGE = 100
+const val MAX_VOLUME_FOR_AVERAGE = 100
