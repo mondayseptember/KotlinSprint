4ec1f0ec8620bd6a891d3c3807306eb7eb43ed64
ilzira <mondayseptember@yandex.ru>
2023-12-16 20:58:38 +0500

    KS-5-4 решение для 4 задачи, в которой проверяется зарегистрирован ли пользователь в системе

---
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -0,0 +1,20 @@
+package lesson_5
+
+fun main() {
+    println("Введите, пожалуйста, свое имя:")
+    val userName = readln()
+    val login = "Zaphod"
+    val password = "PanGalactic"
+
+    if (userName == login) {
+        println("Введите пароль, $userName:")
+        val userPassword = readln()
+        if (userPassword == password) {
+            println("Добро пожаловать!")
+        } else {
+            println("Неверный пароль")
+        }
+    } else {
+        println("Необходимо зарегистрироваться")
+    }
+}
