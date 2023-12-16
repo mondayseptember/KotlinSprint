cb01aaf99d370b1dc67ef9f9751a2af768fa3b2e
ilzira <mondayseptember@yandex.ru>
2023-12-16 14:54:56 +0500

    KS-4-4 удалена лишняя переменная

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,13 +3,13 @@ package lesson_4
 fun main() {
     val dayOfTraining = 5
     val evenDays = dayOfTraining % 2 == 0
-    val oddDays = !evenDays
 
-    println("""  
-    Упражнения для рук:       $oddDays
+    println(
+        """  
+    Упражнения для рук:       ${!evenDays}
     Упражнения для ног:       $evenDays
     Упражнения для спины:  $evenDays
-    Упражнения для пресса: $oddDays
-    """.trimIndent()
+    Упражнения для пресса: ${!evenDays}
+        """.trimIndent()
     )
 }
