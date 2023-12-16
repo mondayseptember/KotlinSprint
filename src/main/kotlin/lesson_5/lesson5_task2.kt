93e6b7ea2eabc786317bd8842c8ebba8800608ac
ilzira <mondayseptember@yandex.ru>
2023-12-16 23:08:08 +0500

    KS-5-2 исправление для 2 задачи

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -1,15 +1,13 @@
 package lesson_5
 
 fun main() {
-    val userYearOfBirth = readln()!!.toInt()
+    println("Введите год рождения:")
+    val userYearOfBirth = readln().toInt()
     val currentYear = 2023
-    val userAge = currentYear - userYearOfBirth
 
-    val resultText = if (userAge >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
-    else if (userAge == 16 || userAge == 17) "Показать экран с ограниченным контентом"
-    else "Вернуться на главный экран"
-
-    println(resultText)
+    if ((currentYear - userYearOfBirth) >= AGE_OF_MAJORITY) {
+        println("Показать экран со скрытым контентом")
+    }
 }
 
 const val AGE_OF_MAJORITY = 18
