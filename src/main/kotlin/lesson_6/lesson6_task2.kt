990ee2b143b6cf8938aed06e3d4839d01b846023
ilzira <mondayseptember@yandex.ru>
2023-12-18 12:49:42 +0500

    KS-6-2 исправление для 2 задачи

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Введите количество секунд, которые нужно засечь:")
     val countOfSec = readln().toInt()
 
-    Thread.sleep(1000)
+    Thread.sleep(1000 * countOfSec.toLong())
 
     println("Прошло $countOfSec секунд")
 }
