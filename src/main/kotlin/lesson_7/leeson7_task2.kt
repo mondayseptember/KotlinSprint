ca4fece39c7cfc7e94157194e9034ec27a022bf2
ilzira <mondayseptember@yandex.ru>
2023-12-19 10:07:25 +0500

    KS-7-2 решение для 2 задачи, которая имитирует авторизацию по коду из смс

---
+++ src/main/kotlin/lesson_7/leeson7_task2.kt
@@ -0,0 +1,11 @@
+package lesson_7
+
+fun main() {
+    do {
+        val smsCode = (1000..9999).random()
+        println("Ваш код авторизации: $smsCode")
+        println("Введите код, чтобы авторизоваться:")
+        val userCode = readln().toInt()
+    } while (userCode != smsCode)
+    println("Добро пожаловать!")
+}
