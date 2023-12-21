a6c19883f6b07561cd7dd4c61d17f8b6db985045
ilzira <mondayseptember@yandex.ru>
2023-12-21 20:24:14 +0500

    KS-8-3 исправлено условие if

--- src/main/kotlin/lesson_8/lesson8_task3.kt
+++ src/main/kotlin/lesson_8/lesson8_task3.kt
@@ -2,14 +2,13 @@ package lesson_8
 
 fun main() {
     val ingredientsForVinegret =
-        arrayOf("свекла", "морковь", "зеленый горощек", "лук", "картофель", "растительное масло", "соль")
+        arrayOf("свекла", "морковь", "зеленый горошек", "лук", "картофель", "растительное масло", "соль")
     println("Введите название ингредиента, который вы хотите найти:")
     val infoAboutIngredients = readln()
 
-    if (ingredientsForVinegret.indexOf(infoAboutIngredients) >= 0) {
+    if (infoAboutIngredients in ingredientsForVinegret) {
         println("Ингредиент \"$infoAboutIngredients\" в рецепте есть")
     } else {
         println("Такого ингредиента в рецепте нет")
     }
 }
-
