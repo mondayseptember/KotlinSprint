97b5703362ab2ca832a4992fc1215423e4a6062c
ilzira <mondayseptember@yandex.ru>
2023-12-21 20:15:58 +0500

    KS-8-1 цикл for заменен на функцию sum

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -2,10 +2,7 @@ package lesson_8
 
 fun main() {
     val numberOfViewsPerDay = arrayOf(111, 113, 110, 222, 98, 342, 301)
-    var numberOfViewsPerWeek = 0
+    val numberOfViewsPerWeek = numberOfViewsPerDay.sum()
 
-    for (i in numberOfViewsPerDay) {
-        numberOfViewsPerWeek += i
-    }
     println(numberOfViewsPerWeek)
 }
