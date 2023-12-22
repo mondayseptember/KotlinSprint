403de29d084410c125e3b8afcbbecf65d3431aff
ilzira <mondayseptember@yandex.ru>
2023-12-23 01:13:12 +0500

    KS-9-4 исправление для 4 задачи

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -2,9 +2,6 @@ package lesson_9
 
 fun main() {
     println("Напишите 5 ингредиентов, перечисленных через “,”")
-    val listOfIngredients = listOf("", "", "", "", "")
-    val listOfIngredientsFromUser: List<String> = listOfIngredients.map {
-        readln()
-    }
-    println(listOfIngredientsFromUser.sorted())
+    val userIngredients = readln().split(", ")
+    println(userIngredients.sorted())
 }
