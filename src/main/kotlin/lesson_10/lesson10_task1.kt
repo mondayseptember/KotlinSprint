ca179652f4c843b174758ea0b85fb569cebe56ec
ilzira <mondayseptember@yandex.ru>
2023-12-25 07:41:51 +0500

    KS-10-1 исправление для 1 задачи

--- src/main/kotlin/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -7,12 +7,10 @@ fun main() {
     println("Кости бросает Пользователь. Количество очков: $userResult")
     println("Кости бросает Компьютер. Количество очков: $computerResult")
 
-    if (userResult > computerResult) {
-        println("Победило человечество")
-    } else if (userResult < computerResult) {
-        println("Победила машина")
-    } else {
-        println("Ничья")
+    when (true) {
+        (userResult > computerResult) -> println("Победило человечество")
+        (userResult < computerResult) -> println("Победила машина")
+        else -> println("Ничья")
     }
 }
 
