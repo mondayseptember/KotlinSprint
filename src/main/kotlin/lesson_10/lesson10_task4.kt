343dd3c52cc85122074059dd46d60d43bb9e1d47
ilzira <mondayseptember@yandex.ru>
2023-12-25 08:35:17 +0500

    KS-10-4 исправление для 4 задачи

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -5,20 +5,13 @@ fun main() {
     do {
         val userResult = throwDice()
         val computerResult = throwDice()
-        printInfoAboutGame("User", "Computer", userResult, computerResult)
-
-        if (userResult > computerResult) {
-            println("Победило человечество")
-            numberOfWin++
-        } else if (userResult < computerResult) {
-            println("Победила машина")
-        } else {
-            println("Ничья")
-        }
+        println("Кости бросает Пользователь. Количество очков: $userResult")
+        println("Кости бросает Компьютер. Количество очков: $computerResult")
+        printGameResult(userResult, computerResult)
+        if (userResult > computerResult) numberOfWin++
 
         println("Хотите бросить кости еще раз? Введите Да или Нет")
     } while (readln().equals("да", ignoreCase = true))
-
     println("Количество выигранных партий: $numberOfWin")
 }
 
@@ -26,7 +19,10 @@ fun throwDice(): Int {
     return (1..6).random()
 }
 
-fun printInfoAboutGame(player1: String, player2: String, resultPlayer1: Int, resultPlayer2: Int) {
-    println("Кости бросает $player1. Количество очков: $resultPlayer1")
-    println("Кости бросает $player2. Количество очков: $resultPlayer2")
+fun printGameResult(userResult: Int, computerResult: Int) {
+    when (true) {
+        (userResult > computerResult) -> println("Победило человечество")
+        (userResult < computerResult) -> println("Победила машина")
+        else -> println("Ничья")
+    }
 }
