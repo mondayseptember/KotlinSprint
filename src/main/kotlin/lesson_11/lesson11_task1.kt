44c45440b3aa8aad3e343896933ff51252fee76b
ilzira <mondayseptember@yandex.ru>
2024-01-07 00:59:17 +0500

    KS-11-1 испраление для 1 задачи

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -1,4 +1,11 @@
-package lesson_11_task1
+package lesson_11
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+)
 
 fun main() {
     val user1 = User(
