892b2fa0de90690af1634a9e528dc57b5cbda2cb
ilzira <mondayseptember@yandex.ru>
2024-01-09 23:03:44 +0500

    KS-13-1 переименовано название класса

--- src/main/kotlin/lesson_13/lesson13_task1.kt
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -1,12 +1,12 @@
 package lesson_13
 
-class PhoneBook(
+class Contact(
     val name: String,
     val phoneNumber: Long,
     val company: String?,
 )
 
 fun main() {
-    val number = PhoneBook("Elena", 83474892367, null)
+    val number = Contact("Elena", 83474892367, null)
     println("имя: ${number.name}, номер телефона: ${number.phoneNumber}, компания: ${number.company}")
 }
