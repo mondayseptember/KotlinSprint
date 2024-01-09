aa0a1e05c23bc2bb3743c7c139b1bb806f175465
ilzira <mondayseptember@yandex.ru>
2024-01-09 23:06:24 +0500

    KS-13-3 переименовано название класса

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -1,18 +1,18 @@
 package lesson_13
 
-class PhoneBook3(
+class Contact3(
     val name: String,
     val phoneNumber: Long,
     val company: String? = null,
 )
 
 fun main() {
-    val listOfExistingCompanies = listOf<PhoneBook3>(
-        PhoneBook3("Bob", 89376573398),
-        PhoneBook3("Tom", 89476573399),
-        PhoneBook3("Sam", 89376573101, "null"),
-        PhoneBook3("Pam", 89116573333, "Flowers"),
-        PhoneBook3("Ron", 89556573398, "Lilart")
+    val listOfExistingCompanies = listOf<Contact3>(
+        Contact3("Bob", 89376573398),
+        Contact3("Tom", 89476573399),
+        Contact3("Sam", 89376573101, "null"),
+        Contact3("Pam", 89116573333, "Flowers"),
+        Contact3("Ron", 89556573398, "Lilart")
     ).filter {
         it.company != null
     }
