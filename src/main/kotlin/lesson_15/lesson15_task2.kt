fd4a75486f8e51948dc59e7d28bb2cfa7afb6da8
ilzira <mondayseptember@yandex.ru>
2024-01-17 01:49:27 +0500

    KS-15-2 исправление для 2 задачи

--- src/main/kotlin/lesson_15/lesson15_task2.kt
+++ src/main/kotlin/lesson_15/lesson15_task2.kt
@@ -11,15 +11,16 @@ class PrecipitationAmount(
 ) : WeatherStationStats()
 
 class WeatherServer {
-    fun sendMessageToServer(temperature: Int, precipitationAmount: Int) {
-        println("Данные температуры: $temperature")
-        println("Данные количества осадков: $precipitationAmount")
+    fun sendMessageToServer(weatherData: WeatherStationStats) {
+        when (weatherData) {
+            is Temperature -> println("Данные температуры: ${weatherData.temperature}")
+            is PrecipitationAmount -> println("Данные количества осадков: ${weatherData.precipitationAmount}")
+        }
     }
 }
 
 fun main() {
-    val weatherData = WeatherServer().sendMessageToServer(
-        Temperature(32).temperature,
-        PrecipitationAmount(420).precipitationAmount
-    )
+    val weatherData = WeatherServer()
+    weatherData.sendMessageToServer(Temperature(33))
+    weatherData.sendMessageToServer(PrecipitationAmount(420))
 }
