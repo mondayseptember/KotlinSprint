ce5ff173bab1e2371e48c8ea564c5d1d9580ad7d
ilzira <mondayseptember@yandex.ru>
2024-01-23 20:48:58 +0500

    KS-19-3 решение 3 задачи, в котором создан космический корабль

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,20 @@
+package lesson_19
+
+class Spaceship {
+
+    fun takeOff() {
+        // TODO("нужна информация о дополнительной логике")
+    }
+
+    fun land() {
+        TODO()
+    }
+
+    fun shootBackFromAsteroid() {}
+}
+
+fun main() {
+    val test = Spaceship()
+    test.takeOff()
+    test.land()
+}
