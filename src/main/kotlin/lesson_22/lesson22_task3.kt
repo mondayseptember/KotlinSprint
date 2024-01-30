15dabb08c8d00a69352e86591eb80da66ed00506
ilzira <mondayseptember@yandex.ru>
2024-01-31 00:18:41 +0500

    KS-22-3 решение 3 задачи, в котором сделана деструктуризация объекта на отдельные переменные

--- src/main/kotlin/lesson_22/lesson22_task3.kt
+++ src/main/kotlin/lesson_22/lesson22_task3.kt
@@ -3,13 +3,13 @@ package lesson_22
 data class Book(
     val name: String,
     val author: String,
-    val numberOfParts: String,
+    val numberOfParts: Int,
 )
 
 fun main() {
-    val book = Book("Harry Potter", "J. K. Rowling", "7")
+    val book = Book("Harry Potter", "J. K. Rowling", 7)
 
-    val(name: String, author: String, numberOfParts: String) = book
+    val(name, author, numberOfParts) = book
 
     println("Название: $name, автор $author, количество частей: $numberOfParts")
 }
