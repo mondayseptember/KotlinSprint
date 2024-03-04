77026b083794980c85909b9321e2a4f9aa904cc4
ilzira <mondayseptember@yandex.ru>
2024-03-04 19:51:55 +0500

    KS-13-5 исправление 5 задачи

--- src/main/kotlin/lesson_13/lesson13_task5.kt
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -1,13 +1,13 @@
 package lesson_13
 
 class Contact5(
-    val name: String = readln(),
-    val phoneNumber: String = readln(),
-    val company: String = readln(),
+    val name: String,
+    val phoneNumber: String,
+    val company: String? = null,
 )
 
 fun main() {
-    val contact = Contact5()
+    val contact = Contact5(readln(), readln())
 
     try {
         contact.phoneNumber.toLong()
