e624e845b9d2f445f823b236e2969f395473c201
ilzira <mondayseptember@yandex.ru>
2024-03-04 18:51:17 +0500

    KS-8-5 решение 5 задачи с помощью лямбды

--- src/main/kotlin/lesson_1_10/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_1_10/lesson_8/lesson8_task5.kt
@@ -1,15 +1,8 @@
 package lesson_1_10.lesson_8
 
 fun main() {
-    println("Введите количество планируемых ингредиентов:")
-    val ingredientsNumber = readln().toInt()
+    println("Введите количество планируемых ингредиентов и ингредиенты:")
+    val ingredientsNumber = Array(readln().toInt()) { readln() }
 
-    var ingredientsArray = arrayOf<String>()
-
-    println("Введите ингредиенты:")
-    for (i in 1..ingredientsNumber) {
-        ingredientsArray += readln()
-    }
-
-    println(ingredientsArray.size)
+    println(ingredientsNumber.joinToString(", "))
 }
